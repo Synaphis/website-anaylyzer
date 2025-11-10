21aba5178c19bc6f8995490a329af54ee7f61011
Mujtaba Javaid <mujtabajavaid@synaphis.com>
2025-11-11 00:25:00 +0500

    Fixed Deployment Errors

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 echo "Installing Puppeteer bundled Chromium..."
+# installs Chromium used by puppeteer in CI hosts
 npx puppeteer install
+echo "Done puppeteer install"
